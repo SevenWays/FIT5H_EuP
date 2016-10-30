f43202d63dc11a3b9b049187ed2da4bf54881759
SewenWays <ellips.project@gmail.com>
2016-10-30 11:45:59 +0100

    New

--- Ruby/blocke.rb
+++ Ruby/blocke.rb
@@ -1,3 +1,48 @@
+# Aufgabe 1
 numbers = (15..20).to_a
 
-numbers.each_index{|a| puts "Index: #{a} #{numbers.each{|v| puts "- Wert #{v}"}"}"
+# Aufgabe 1a
+puts "////////////////// Aufgabe 1a //////////////////////"
+numbers.each { |number|
+puts number.to_s
+ }
+
+ # Aufgabe 1b
+puts "////////////////// Aufgabe 1b //////////////////////"
+
+numbers.each { |number|
+puts "Index: " + numbers.index(number).to_s + " - Wert: " + number.to_s
+ }
+
+# Aufgabe 1c
+puts "////////////////// Aufgabe 1c //////////////////////"
+
+hash1 = Hash.new
+numbers.each { |number|
+hash1[number] = rand(100..200)
+ }
+
+# Aufgabe 1d
+puts "////////////////// Aufgabe 1d //////////////////////"
+
+hash1.each { |key, value|
+puts "Index: " + key.to_s + " - Wert: " + value.to_s
+ }
+
+# Aufgabe 1e
+puts "////////////////// Aufgabe 1e //////////////////////"
+
+hash1.each { |key, value|
+str = "Index: " + key.to_s + " - Wert: " + value.to_s
+if value > 150
+  str = str + " WOW!!!"
+end
+puts str
+ }
+
+ # Aufgabe 1f
+puts "////////////////// Aufgabe 1f //////////////////////"
+
+ numbers.each { |number|
+
+}
