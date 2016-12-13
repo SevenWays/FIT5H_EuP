e9e19ba7cd8f2a00d7da377212563a64a62128ab
SevenWays <info@seven-ways.net>
2016-12-13 13:07:27 +0100

    13.12.16

---
+++ Rails/Projekte/rails_app/app/models/event.rb
@@ -0,0 +1,9 @@
+class Event < ApplicationRecord
+	validates :name, :description, :location, presence: true
+	validates :description, length: { minimum: 25 }
+	validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+	def free?
+		price.blank? || price.zero?
+	end
+end
